bd183dc463829a5d09bdb756f1454eca14a7e060
Dzulhilmi <=>
2023-08-18 23:25:54 +0700

    implement feat

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -16,9 +16,9 @@ import { Textarea } from "../ui/textarea";
 
 import { usePathname, useRouter } from "next/navigation";
 
-import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.action";
+import { useOrganization } from "@clerk/nextjs";
 
 interface Props {
   user: {
@@ -35,6 +35,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -48,7 +49,7 @@ function PostThread({ userId }: { userId: string }) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
